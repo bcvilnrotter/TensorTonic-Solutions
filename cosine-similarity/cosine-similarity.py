770eb6717fbb33da6bf5d719415cd6498997fe8e
bcvilnrotter <35104906+bcvilnrotter@users.noreply.github.com>
2026-02-09 08:17:30 -0500

    Update solution for cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -5,6 +5,9 @@ def cosine_similarity(a, b):
     Compute cosine similarity between two 1D NumPy arrays.
     Returns: float in [-1, 1]
     """
+    def normalize(a):
+      return np.linalg.norm(a)
+    
     A,B = np.array(a),np.array(b)
-    mag_A,mag_B = np.linalg.norm(A),np.linalg.norm(B)
+    mag_A,mag_B = normalize(A),normalize(B)
     return (A @ B) / (mag_A * mag_B) if mag_A != 0 and mag_B != 0 else 0
